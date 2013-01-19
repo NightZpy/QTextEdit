1cfe0a4f88acde01a2ab89f37d4102da03f0ec71
Lenyn <nightzpy@gmail.com>
2013-01-19 03:18:09 -0430

    mainwindow class defined
    Add all vars and methods need.

--- mainwindow.h
+++ mainwindow.h
@@ -3,9 +3,65 @@
 
 #include <QtGui/QMainWindow>
 
+class QAction;
+class QPlainTextEdit;
+class QMenu;
+class QToolBar;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    void about();
+    void documentWasModified();
+
+private:
+    //Atributtes
+    QString *curFile;
+    QPlainTextEdit *textEdit;
+
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *helpMenu;
+
+    QToolBar *fileToolBar;
+    QToolBar *editToolBar;
+
+    QAction *newAction;
+    QAction *openAction;
+    QAction *saveAction;
+    QAction *saveAsAction;
+    QAction *exitAction;
+    QAction *cutAction;
+    QAction *copyAction;
+    QAction *pasteAction;
+    QAction *aboutAction;
+    QAction *aboutQtAction;
+
+    //Methods
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void createStatusBar();
+
+    void readSettings();
+    void writeSettings();
+
+    bool maybeSave();
+    void loadFile(const QString &fileName);
+    bool saveFile(const QString &fileName);
+
+    void setCurrentFile(const QString &fileName);
+    QString strippedName(const QString &fullFileName);
     
 public:
     MainWindow(QWidget *parent = 0);
